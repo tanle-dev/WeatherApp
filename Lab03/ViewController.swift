03bb164baf371679192db8863824229ffd4aff69
tanle <lenhuttan.programing@gmail.com>
2024-03-11 18:03:27 -0400

    Create basic UI

---
+++ Lab03/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab03
+//
+//  Created by Tan Le on 2024-03-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
